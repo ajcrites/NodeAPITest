c4a16091a8235ea53692cbf680fbf7ac0773a1b7
Andrew Crites <acrites@mobiquityinc.com>
2023-11-07 21:50:32 -0500

    fix: write to the text files in the correct order.
    The while loop cannot be asynchronous, so the i++ was done to correctly loop through data. However, fs.appendFile is asynchronous, so all of the write actions are queued immediately until the loop resolves. Node cannot guarantee that these async actions will be done in a particular order, so you must wait until one write is complete before queuing the next write.
    
    The simplest way to solve this is to use appendFileSync, which is blocking -- the loop will not increment until the current write is complete.
    
    Also starting clearing the files when the application runs.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const data = require('./utils/download_data.js')
 const filter = require('./utils/filter.js')
 
 async function main() {
+  fs.truncateSync('data.JSON');
+  fs.truncateSync('filteredData.txt');
+  fs.truncateSync('filteredDataReversed.txt');
   await data.request();
   filter.filterData();
 }
--- utils/filter.js
+++ utils/filter.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 
 
 const loadData = () => {
-    
     try {
         const dataBuffer = fs.readFileSync('data.json')
         const dataJSON = dataBuffer.toString()
@@ -11,50 +10,34 @@ const loadData = () => {
     } catch(e) {
         return []
     }
-    
 }
 
 const filterData = () => {
-    console.log("In filtered data")
-
     const data = loadData();
-    var filteredData = [];
+    const filteredData = [];
 
     data.forEach((el) => {
         filteredData.push(el.login + " " + el.id)
     })
-    console.log(filteredData)
     saveFiltered(filteredData);
-
 }
 
 const saveFiltered = (data) => {
-    
     let i = 0;
-    while ( i < data.length) {
-        fs.appendFile('filteredData.txt', data[i] + "\n", function (err) {
-        if (err) throw err});
-
+    while (i < data.length) {
+        fs.appendFileSync('filteredData.txt', `${data[i]}\n`);
         i++;
-        console.log("in while loop")
-     }
+    }
     reverseFiltered(data);
 }
 
 const reverseFiltered = (data) => {
     let i = 0;
     while ( i < data.length) {
-        
-        var reversedElement = data[i].split("").reverse().join("");
-
-        fs.appendFile('filteredDataReversed.txt', reversedElement + "\n", function (err) {
-        if (err) throw err});
-
+        const reversedElement = data[i].split("").reverse().join("");
+        fs.appendFileSync('filteredDataReversed.txt', `${reversedElement}\n`);
         i++;
-        console.log("in reverse while loop")
     }
 }
-
-
 
 module.exports = {filterData};
